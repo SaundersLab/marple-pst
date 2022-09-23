76d3ee600af93c4ec60669acd36cce2c6ad9937b
tonytony99 <anthony.bryan@outlook.com>
2022-09-23 14:54:10 +0100

    ensure possible flows through genotype calculation are tested

--- test/unit/test_unit.py
+++ test/unit/test_unit.py
@@ -29,30 +29,41 @@ class TestTransform(unittest.TestCase):
             # ?	        3+ bases qualified	                                T ACT ? 0.2,0.6,0.2
         cases = [
             # 0/0 : pref ≥ hetero_max or only refbase qualified
-            ('A', ['A'],         ['1'],                 .2, .8, '0/0', 'AA',   '1'),
-            ('A', ['A', 'T'],    ['0.9', '0.1'],        .2, .8, '0/0', 'AA',   '0.9'),
-            ('A', ['T', 'A'],    ['0.1', '0.9'],        .2, .8, '0/0', 'AA',   '0.9'),
+            ('A', ['A'],           ['1'],                   .2, .8, '0/0', 'AA',   '1'),
+            ('A', ['A', 'T'],      ['0.9', '0.1'],          .2, .8, '0/0', 'AA',   '0.9'),
+            ('A', ['T', 'A'],      ['0.1', '0.9'],          .2, .8, '0/0', 'AA',   '0.9'),
             # 0/1 : ref base and 1 alt base qualified
-            ('T', ['C', 'T'],    ['0.217', '0.783'],    .2, .8, '0/1', 'CT',   '0.217,0.783'),
-            ('A', ['A', 'T'],    ['0.5', '0.5'],        .2, .8, '0/1', 'AT',   '0.5,0.5'),
-            ('A', ['T', 'A'],    ['0.5', '0.5'],        .2, .8, '0/1', 'TA',   '0.5,0.5'),
+            ('T', ['C', 'T'],      ['0.217', '0.783'],      .2, .8, '0/1', 'CT',   '0.217,0.783'),
+            ('A', ['A', 'T'],      ['0.5', '0.5'],          .2, .8, '0/1', 'AT',   '0.5,0.5'),
+            ('A', ['T', 'A'],      ['0.5', '0.5'],          .2, .8, '0/1', 'TA',   '0.5,0.5'),
             # 1/1 : palt 1 ≥ hetero_max or only 1 alt base qualified
-            ('T', ['T', 'C'],    ['0.178', '0.822'],    .2, .8, '1/1', 'CC',   '0.822'),
-            ('A', ['A', 'T'],    ['0.1', '0.9'],        .2, .8, '1/1', 'TT',   '0.9'),
-            ('T', ['A', 'T'],    ['0.9', '0.1'],        .2, .8, '1/1', 'AA',   '0.9'),
+            ('T', ['T', 'C'],      ['0.178', '0.822'],      .2, .8, '1/1', 'CC',   '0.822'),
+            ('A', ['A', 'T'],      ['0.1', '0.9'],          .2, .8, '1/1', 'TT',   '0.9'),
+            ('T', ['A', 'T'],      ['0.9', '0.1'],          .2, .8, '1/1', 'AA',   '0.9'),
+            ('A', ['A', 'T', 'G'], ['0.15', '0.15', '0.7'], .2, .8, '1/1', 'GG',   '0.7'),
             # 1/2 : 2 alt bases qualified and ref base did not
-            ('C', ['A', 'T'],    ['0.476', '0.524'],    .2, .8, '1/2', 'AT',   '0.476,0.524'),
-            ('A', ['T', 'C'],    ['0.5', '0.5'],        .2, .8, '1/2', 'TC',   '0.5,0.5'),
+            ('C', ['A', 'T'],      ['0.476', '0.524'],      .2, .8, '1/2', 'AT',   '0.476,0.524'),
+            ('A', ['T', 'C'],      ['0.5', '0.5'],          .2, .8, '1/2', 'TC',   '0.5,0.5'),
             # ? : 3+ bases qualified
-            ('T', ['A','C','T'], ['0.2', '0.6', '0.2'], .2, .8, '?',   'ACT',  '0.2,0.6,0.2'),
-            ('A', ['A','C','T'], ['0.3', '0.4', '0.3'], .2, .8, '?',   'ACT',  '0.3,0.4,0.3'),
-            ('G', list('ACTG'),  ['0.25'] * 4,          .2, .8, '?',   'ACTG', '0.25,0.25,0.25,0.25'),
+            ('T', ['A', 'C', 'T'], ['0.2', '0.6', '0.2'],   .2, .8, '?',   'ACT',  '0.2,0.6,0.2'),
+            ('A', ['A', 'C', 'T'], ['0.3', '0.4', '0.3'],   .2, .8, '?',   'ACT',  '0.3,0.4,0.3'),
+            ('G', list('ACTG'),  ['0.25'] * 4,              .2, .8, '?',   'ACTG', '0.25,0.25,0.25,0.25'),
         ]
         for ref, bases, ratio_strs, hetero_min, hetero_max, genotype, valid_bases, valid_ratios in cases:
             self.assertEqual(
                 _get_genotype_and_valid_bases_and_valid_ratios(ref, bases, ratio_strs, hetero_min, hetero_max),
                 (genotype, valid_bases, valid_ratios)
             )
+
+        # No bases qualified
+        none_cases = [
+            # no ratios qualified
+            ('G', list('ACTG'),     ['0.25'] * 4,            .3, .7),
+            # only ref has min <= freq <= max
+            ('A', ['A', 'T', 'G'],  ['0.7', '0.15', '0.15'], .2, .8),
+        ]
+        for ref, bases, ratio_strs, hetero_min, hetero_max in none_cases:
+            self.assertIsNone(_get_genotype_and_valid_bases_and_valid_ratios(ref, bases, ratio_strs, hetero_min, hetero_max))
 
 
 if __name__ == '__main__':
