f9ba3f3029deea600444a4670c5c8fb9dc9f8ebe
tonytony99 <anthony.bryan@outlook.com>
2022-09-28 15:21:31 +0100

    set the correct path to the conda installer to delete

--- install/uninstall.sh
+++ install/uninstall.sh
@@ -10,5 +10,5 @@ source "$marple_pst_dir"/marple_pst_miniconda/bin/activate
 for i in $(seq ${CONDA_SHLVL}); do conda deactivate ; done
 conda env remove -n marple-pst
 
-rm "$marple_pst_dir"/marple_pst_miniconda.sh
+rm "$install_dir"/marple_pst_miniconda.sh
 rm -rf "$marple_pst_dir"/marple_pst_miniconda
