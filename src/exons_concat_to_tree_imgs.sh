00b32a9e51c985492a10d3335b93edcd5348e28f
tonytony99 <anthony.bryan@outlook.com>
2022-09-30 16:27:02 +0100

    add bash strict mode

--- src/exons_concat_to_tree_imgs.sh
+++ src/exons_concat_to_tree_imgs.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+# Bash strict mode
+set -euo pipefail
+
 src_dir=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 marple_pst_dir=$( dirname "$src_dir" )
 source "$marple_pst_dir"/marple_pst_miniconda/bin/activate marple-pst
