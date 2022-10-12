2d758af3677daad6cead7879db49aee937aa501d
tonytony99 <anthony.bryan@outlook.com>
2022-10-12 14:40:26 +0100

    fixes #24 use SLURM_JOBID to find script locations allowing script to be run with sbatch

--- src/reads_to_exons_concat.sh
+++ src/reads_to_exons_concat.sh
@@ -1,9 +1,14 @@
 #!/bin/bash
 
+[ -z $SLURM_JOBID ] && is_on_slurm=false || is_on_slurm=true
+
+$is_on_slurm || script=$0
+$is_on_slurm && script=$(scontrol show job $SLURM_JOBID | awk -F= '/Command=/{print $2}' | cut -d" " -f1)
+src_dir=$(cd $(dirname "$script"); pwd)
+
 # Bash strict mode
 set -euo pipefail
 
-src_dir=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 marple_pst_dir=$( dirname "$src_dir" )
 source "$marple_pst_dir"/marple_pst_miniconda/bin/activate marple-pst
 python3 "$src_dir"/reads_to_exons_concat.py $@
